26059759a95634b0fae60dac3b52061c15fa8268
CarolBV <carolina.botella.valero@gmail.com>
2024-09-03 12:37:20 +0200

    add configuration security

--- src/main/java/com/fotografia/fotografia/config/SecurityConfig.java
+++ src/main/java/com/fotografia/fotografia/config/SecurityConfig.java
@@ -2,11 +2,9 @@ package com.fotografia.fotografia.config;
 
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +12,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fotografia.fotografia.services.AdminDetailsService;
 
 
 
@@ -21,8 +23,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
   
-    private final AdminDetailsService AdminDetailsService;
-    public SecurityConfig(UserDetailsService userDetailsService) {
+    public SecurityConfig(UserDetailsService userDetailsService, AdminDetailsService adminDetailsService) {
         
     }
   @Bean
@@ -32,8 +33,14 @@ public class SecurityConfig {
     .requestMatchers("/gallery/**").hasRole("ADMIN")
     .anyRequest().authenticated()
     )
-    .formLogin(withDefaults())
-    .logout(withDefaults());
+    .formLogin(form -> form
+    .loginPage("/login") // Puedes definir una página de inicio de sesión personalizada si lo deseas
+    .permitAll()
+)
+.logout(logout -> logout
+    .logoutUrl("/logout")
+    .permitAll()
+);
     return http.build();
   }
   @Bean
@@ -43,5 +50,18 @@ public class SecurityConfig {
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
     return authenticationConfiguration.getAuthenticationManager();
+  }
+  @Bean
+  public WebMvcConfigurer corsConfigurer(CorsRegistry registry) {
+    return new WebMvcConfigurer() {
+        @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+    };
   }
 }
